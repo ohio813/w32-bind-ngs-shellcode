5e2dfef462ad5337d012a6489252a83d0f269c49
berendjanwever <berendjanwever@a0d5343a-76d3-11de-9803-113d0ae2e04d>
2009-07-24 09:52:25 +0000

    -Windows 5.0-6.0 +Windows 5.0-7.0

--- w32-bind-ngs-shellcode.asm
+++ w32-bind-ngs-shellcode.asm
@@ -1,5 +1,5 @@
 BITS 32
-; Windows x86 null-free bindshell for Windows 5.0-6.0 all service packs.
+; Windows x86 null-free bindshell for Windows 5.0-7.0 all service packs.
 ; (See http://skypher.com/wiki/index.php/Hacking/Shellcode/Bind/NGS).
 ; Based largely on code and ideas (C) 2005 by Dafydd Stuttard, NGS Software.
 ; (See http://www.ngssoftware.com/papers/WritingSmallShellcode.pdf).
