f0ac19567c728a60f8ca858f39bf492353f17c9a
berendjanwever <berendjanwever@a0d5343a-76d3-11de-9803-113d0ae2e04d>
2009-07-26 13:10:45 +0000

    Add support for pre-initialized hashes used in the latest version of the shellcode.

--- hash.py
+++ hash.py
@@ -8,8 +8,8 @@ def LoadExports(module_name):
     exports.append(export)
   return exports
 
-def GetHash(s, x):
-  (AL, AH) = (0, 0)
+def GetHash(s, x, y):
+  (AL, AH) = (0, y)
   for c in (s+"\0"):
     AL = ord(c)
     AL ^= x
@@ -17,53 +17,77 @@ def GetHash(s, x):
     AH &= 0xFF
   return AH
 
-def ReportHashes(procs, hash, x):
+def ReportHashes(procs, hash, x, y):
+  print '%-39s equ 0x%02X' % ("hash_xor_value", x)
+  print '%-39s equ 0x%02X' % ("hash_start_value", y)
   for proc in procs:
-    proc_hash = hash(proc, x)
-    print '"%s" = %02X' % (proc, proc_hash)
+    (module, function) = proc.split(".");
+    proc_hash = hash(function, x, y)
+    equ_name = "hash_%s_%s" % (module, function)
+    print '%-39s equ 0x%02X' % (equ_name, proc_hash)
 
-def CheckHashes(exports, procs, hash, x, report_failures = False):
+def CheckHashes(exports, procs, hash, x, y, report_failures = False):
   for proc in procs:
-    proc_hash = hash(proc, x)
-    for export in exports:
-      export_hash = hash(export, x)
+    (module, function) = proc.split(".");
+    proc_hash = hash(function, x, y)
+    for export in exports[module]:
+      export_hash = hash(export, x, y)
       if export_hash == proc_hash:
-        if export != proc:
+        if export != function:
           if report_failures:
-            print '"%s" collides with "%s"' % (proc, export)
+            print '"%s" collides with "%s"' % (function, export)
           return False
         else:
           break
     else:
-      raise Exception("Function \"%s\" not exported!" % proc)
+      raise Exception("Function \"%s\" not exported!" % function)
   return True
 
+def ParseArg(s):
+  if s[0:2] == "0x":
+    return int(s[2:], 16)
+  if s[0] == "'":
+    return ord(s[1])
+  return int(s)
+
 def Main():
-  kernel32_exports = LoadExports("kernel32")
-  ws2_32_exports = LoadExports("ws2_32")
-  kernel32_procs = ["CreateProcessA", "LoadLibraryA"]
-  ws2_32_procs = ["WSAStartup", "WSASocketA", "bind", "listen", "accept"]
-  if len(sys.argv) == 2:
+  exports = {
+      "kernel32": LoadExports("kernel32"),
+      "ws2_32": LoadExports("ws2_32")
+  }
+  procs = ["kernel32.CreateProcessA", "kernel32.LoadLibraryA",
+      "ws2_32.WSAStartup", "ws2_32.WSASocketA", "ws2_32.bind", 
+      "ws2_32.listen", "ws2_32.accept"]
+  if len(sys.argv) == 3:
+    x = ParseArg(sys.argv[1]);
+    y = ParseArg(sys.argv[2]);
+    print "--- %02X ---" % x
+    print "* %02X" % y
+    if CheckHashes(exports, procs, GetHash, x, y, True):
+      ReportHashes(procs, GetHash, x, y)
+  elif len(sys.argv) == 2:
     # Accept one argument: a decimal or hexadecimal number that is used by the
     # hashing algorithm:
-    if sys.argv[1][0:2] == "0x":
-      x = int(sys.argv[1][2:], 16)
-    else:
-      x = int(sys.argv[1])
+    x = ParseArg(sys.argv[1]);
     print "--- %02X ---" % x
-    if (CheckHashes(kernel32_exports, kernel32_procs, GetHash, x, True) and
-        CheckHashes(ws2_32_exports, ws2_32_procs, GetHash, x, True)):
-      ReportHashes(kernel32_procs, GetHash, x)
-      ReportHashes(ws2_32_procs, GetHash, x)
+    for y in range(0, 0x100):
+      if CheckHashes(exports, procs, GetHash, x, y, True):
+        print "* %02X" % y
+        ReportHashes(procs, GetHash, x, y)
   else:
     # If no argument is specified, search for all number that do not give
     # collisions:
     for x in range(0, 0x100):
-      if (CheckHashes(kernel32_exports, kernel32_procs, GetHash, x) and
-          CheckHashes(ws2_32_exports, ws2_32_procs, GetHash, x)):
-        print "--- %02X ---" % x
-        ReportHashes(kernel32_procs, GetHash, x)
-        ReportHashes(ws2_32_procs, GetHash, x)
+      bHeaderShown = False
+      print "--- %02X ---\r" % x,
+      for y in range(0, 0x100):
+        if CheckHashes(exports, procs, GetHash, x, y):
+          if (not bHeaderShown):
+            print "--- %02X ---" % x
+            bHeaderShown = True
+          print "%02X " % y,
+      if (bHeaderShown):
+        print ""
 
 
 if __name__ == "__main__":
